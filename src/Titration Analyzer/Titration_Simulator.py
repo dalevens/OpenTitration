b6b97224c9d961f7c4fba205fc21a36eb33e60f1
jkelowitt <58435724+jkelowitt@users.noreply.github.com>
2021-02-06 19:52:22 -0700

    Removed unused functions. This includes all the write_data functions and the deriv function. The imports related to these changes were removed.

--- src/Titration Analyzer/Titration_Simulator.py
+++ src/Titration Analyzer/Titration_Simulator.py
@@ -2,8 +2,6 @@ import time
 import sys
 from numpy import array, arange, divide, where, flip, abs
 from numpy.core.fromnumeric import prod, sum, transpose
-from pandas import DataFrame
-from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 
 
 def pk_to_k(pk):
@@ -122,25 +120,6 @@ class Bjerrum(AcidBase):
         else:
             return array(alphas)
 
-    def write_alpha_data(self, title="Alpha Value Data", file_headers=False, species_names=None):
-        # Initialize the dataframe with the ph values
-        data_dict = {"pH": self.ph}
-
-        # Add the alpha values for each analyte species
-        if species_names is None:  # If names are not specified, just use generics.
-            for num, alpha in enumerate(transpose(self.alpha_analyte)):
-                data_dict[f"alpha{num}"] = alpha
-        else:  # If names are specified, use them.
-            for num, alpha in enumerate(transpose(self.alpha_analyte)):
-                try:
-                    data_dict[species_names[num]] = alpha
-                except IndexError:
-                    raise ValueError("You have not supplied enough species names!")
-
-        # Make and write the data frame to a csv
-        data = DataFrame(data_dict)
-        data.to_csv(f"{title}.csv", index=False, header=file_headers)
-
 
 class Titration(Bjerrum):
     """
@@ -221,14 +200,6 @@ class Titration(Bjerrum):
         volume = phi * self.volume_analyte * self.concentration_analyte / self.concentration_titrant
         return volume, phi
 
-    def write_titration_data(self, title="Titration Curve Data", file_headers=False):
-        # Make dataframe.
-        pH, volume = self.trim_values(self.ph, self.volume_titrant)
-        data = DataFrame({"volume": volume, "pH": pH})
-
-        # Write to a csv.
-        data.to_csv(f"{title}.csv", index=False, header=file_headers)
-
     def find_buffer_points(self):
         pH, volume = self.trim_values(self.ph, self.volume_titrant)
         pKas = array(self.pk_analyte)
@@ -249,57 +220,8 @@ class Titration(Bjerrum):
 
         return list(volume[points]), list(pH[points])
 
-    def deriv(self, degree):
-        pH, volume = self.trim_values(self.ph, self.volume_titrant)
-
-        # An object which makes splines
-        spline_maker = IUS(volume, pH)
-
-        # An object which calculates the derivative of those splines
-        deriv_function = spline_maker.derivative(n=degree)
-
-        # Calculate the derivative at all of the splines
-        d = deriv_function(volume)
-
-        return volume, d
 
     @staticmethod
     def _scale_data(data, a):
         """abs normalization"""
         return a * (data / (1 + abs(data)))
-
-    def write_analysis_data(self, title="Analysis Data", file_headers=False):
-        # Make dataframe.
-        pH, volume = self.trim_values(self.ph, self.volume_titrant)
-        volumeD1, deriv1 = self.deriv(1)
-        volumeD2, deriv2 = self.deriv(2)
-        volumeEq, pHEq = self.find_equiv_points()
-        volumeBf, pHBf = self.find_buffer_points()
-
-        analysis_row_labels = [
-            *[f"eq pt {n}" for n in range(1, len(volumeEq) + 1)],
-            *[f"bf pt {n}" for n in range(1, len(volumeBf) + 1)],
-        ]
-
-        analysis_volumes = [*[n for n in volumeEq], *[n for n in volumeBf]]
-        analysis_pHs = [*[n for n in pHEq], *[n for n in pHBf]]
-
-        while len(analysis_row_labels) < len(deriv1):
-            analysis_row_labels.append(None)
-            analysis_volumes.append(None)
-            analysis_pHs.append(None)
-
-        data = DataFrame(
-            {
-                "volume": volume,
-                "1st Derivative": deriv1,
-                "2nd Derivative": deriv2,
-                " ": [" " for _ in range(len(deriv1))],
-                "Analysis": analysis_row_labels,
-                "Volume at Point": analysis_volumes,
-                "pH at Point": analysis_pHs,
-            }
-        )
-
-        # Write to a csv.
-        data.to_csv(f"{title}.csv", index=False, header=True)
