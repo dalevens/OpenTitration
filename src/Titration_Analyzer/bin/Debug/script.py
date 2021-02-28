7fbf84ad741a9ce498cba7037bb24921ed008b6b
jkelowitt <jkelowitt@protonmail.com>
2021-02-28 14:32:26 -0700

    Changed titrant acidity variable
    Now defined as "not a_acidic" instead of being passed its variable.

--- src/Titration_Analyzer/bin/Debug/script.py
+++ src/Titration_Analyzer/bin/Debug/script.py
@@ -1,7 +1,8 @@
 from Titration_Simulator import *
+import sys
 
 a_acidic = bool(sys.argv[1])
-t_acidic = bool(sys.argv[2])
+t_acidic = not a_acidic
 
 a_pK = list(map(float, sys.argv[3].split(",")))
 t_pK = list(map(float, sys.argv[4].split(",")))
@@ -12,21 +13,21 @@ t_conc = float(sys.argv[7])
 
 resolution = float(sys.argv[8])
 
-analyte = Compound("analyte", a_acidic, a_pK)
-titrant = Compound("titrant", t_acidic, t_pK)
 
+analyte = Compound(name="analyte", acidic=a_acidic, pKs=a_pK)
+titrant = Compound(name="titrant", acidic=t_acidic, pKs=t_pK)
 
-
-titr = Titration(analyte=analyte, 
-                 titrant=titrant, 
-                 volume_analyte=a_vol, 
-                 concentration_analyte=a_conc, 
+titr = Titration(analyte=analyte,
+                 titrant=titrant,
+                 volume_analyte=a_vol,
+                 concentration_analyte=a_conc,
                  concentration_titrant=t_conc,
                  precision=resolution
-       )
+                 )
 
 pH_values = titr.ph_t
 vol_values = titr.volume_titrant_t
 
-print(pH_values)
-print(vol_values)
+
+print(f"{pH_values}")
+print(f"{vol_values}")
